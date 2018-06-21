634fcf4ebec47df72f8c4fe16f61acf2c69f33f7
Brayan131 <brayan@live.at>
2018-06-22 00:31:42 +0200

    -.-

--- BlinkTheLed/BlinkTheLed/ListViewPage1.xaml.cs
+++ BlinkTheLed/BlinkTheLed/ListViewPage1.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
+using Acr.Reflection;
 using Android.Bluetooth;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -13,7 +14,9 @@ namespace BlinkTheLed
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ListViewPage1 : ContentPage
     {
-        private ObservableCollection<string> Items { get; set; }
+       // private ObservableCollection<DeviceInformation> Items { get; set; }
+        private  ObservableCollection<string> Items { get; set; }
+
         private readonly BluetoothAdapter _manager;
         private ICollection<BluetoothDevice> _bondedDevices;
 
@@ -27,29 +30,41 @@ namespace BlinkTheLed
             
             int i = 0;
 
-            string[] _listMAC = new string[_bondedDevices.Count];
+            string[] _listDevice = new string[_bondedDevices.Count];
+            // List<DeviceInformation> list = new List<DeviceInformation>();
+            // DeviceInformation deviceInformation = new DeviceInformation();
 
-            foreach (var item in _bondedDevices)
+            foreach (var device in _bondedDevices)
             {
-                _listMAC[i] = item.Name  + "   -   " + item.Address;
+                _listDevice[i] = device.Name  + "   -   " + device.Address;
+                
+             // deviceInformation._deviceName = item.Name;
+             // deviceInformation._deviceMAC = item.Address;
+
+            //  list.Add(deviceInformation);
                 i++;
             }
-            
-            Items = new ObservableCollection<string>(_listMAC);
+
+          //  Items = new ObservableCollection<DeviceInformation>(list);
+            Items = new ObservableCollection<string>(_listDevice);
 			
 			MyListView.ItemsSource = Items;
         }
 
 
+        private class DeviceInformation
+        {
+            public string _deviceName { get; set; }
+            public string _deviceMAC { get; set; }
+        }
+        
 
         private async void Handle_ItemTapped(object sender, ItemTappedEventArgs e)
         {
             if (e.Item == null)
                 return;
 
-            
-
-            await DisplayAlert("Item Tapped", $"An item was tapped.", "OK");
+            await DisplayAlert("Item Tapped", $"An item was tapped. - {e.Item} Hash: {e.GetHashCode()}", "OK");
 
             //Deselect Item
             ((ListView)sender).SelectedItem = null;
